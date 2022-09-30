fd60d8823413957f050e27ea6866e65e488d126a
Jelo Quito <jeloquito.dev@gmail.com>
2022-09-30 16:01:00 +0800

    added default filter

---
+++ src/main/java/com/playground/jeq/springtestapp/Config/RequestResponseLoggingFilter.java
@@ -0,0 +1,26 @@
+package com.playground.jeq.springtestapp.Config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class RequestResponseLoggingFilter implements Filter {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(RequestResponseLoggingFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        LOGGER.info("Starting a transaction for req : {}", request.getMethod() + " : " + request.getRequestURI());
+
+        chain.doFilter(request, response);
+
+        LOGGER.info("Committing a transaction for req : {}", request.getMethod() + " : " +  request.getRequestURI());
+    }
+}
