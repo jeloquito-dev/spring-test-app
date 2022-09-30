77829fcd69ab5cff3f14ae32c4549ca914da4d77
Jelo Quito <jeloquito.dev@gmail.com>
2022-09-30 15:40:47 +0800

    modified test api

---
+++ src/main/java/com/playground/jeq/springtestapp/Model/BaseResponse.java
@@ -0,0 +1,29 @@
+package com.playground.jeq.springtestapp.Model;
+
+
+public class BaseResponse<T> {
+
+    private String requestId;
+    private T request;
+
+    public BaseResponse(String requestId, T request) {
+        this.requestId = requestId;
+        this.request = request;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public T getRequest() {
+        return request;
+    }
+
+    public void setRequest(T request) {
+        this.request = request;
+    }
+}
