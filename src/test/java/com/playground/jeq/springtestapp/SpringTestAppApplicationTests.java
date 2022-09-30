5dc6a7f50b247b4973bdc9a4e7b2c6950a93862c
Jelo Quito <jeloquito.dev@gmail.com>
2022-09-30 13:57:23 +0800

    initial commit

---
+++ src/test/java/com/playground/jeq/springtestapp/SpringTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.jeq.springtestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
